29b72275447c759e1e2d295fc7a2762cc807dafe
Mít <hoanglonglpgxxx@gmail.com>
2023-02-26 17:35:03 +0700

    upcode

--- assets/js/career.js
+++ assets/js/career.js
@@ -9,10 +9,11 @@ modalBody = '<div class="success-checkmark">' +
     '</div ></div >';
 modalFooter = '<a href="javascript:void(0)" class="bg-info p-3 rounded color-white fw-bold" onclick="$(this).parents(\'.modal\').removeClass(\'show\')"> Đồng ý </a>';
 $(document).ready(function () {
-    $("#formContact").validate({
+    $("#careerForm").validate({
         rules: {
-            "fields[title]": {
+            "fields[file]": {
                 required: true,
+                fileSize: 10,
             },
             "fields[phone]": {
                 required: true,
@@ -20,20 +21,14 @@ $(document).ready(function () {
             "fields[name]": {
                 required: true,
             },
-            "fields[note]": {
-                required: true,
-            },
             "fields[email]": {
                 required: true,
                 email: true,
             },
-            "fields[services]": {
-                required: true,
-            },
         },
         messages: {
-            "fields[title]": {
-                required: "Bạn chưa nhập tiêu đề",
+            "fields[file]": {
+                required: "Bạn chưa nhập đơn ứng tuyển",
             },
             "fields[phone]": {
                 required: "Bạn chưa nhập số điện thoại",
@@ -41,19 +36,13 @@ $(document).ready(function () {
             "fields[name]": {
                 required: "Bạn chưa nhập họ và tên",
             },
-            "fields[note]": {
-                required: "Bạn chưa nhập ghi chú",
-            },
             "fields[email]": {
                 required: "Bạn chưa nhập địa chỉ email",
                 email: "Vui lòng nhập địa chỉ email chính xác"
             },
-            "fields[services]": {
-                required: "Bạn chưa chọn dịch vụ",
-            },
         },
     });
-    $("#formContact").submit(function (e) {
+    $("#careerForm").submit(function (e) {
         e.preventDefault();
         if ($(this).valid()) {
             $('body').append(modal);
